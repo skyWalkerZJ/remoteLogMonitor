a442b8c5df0e49bd57f814b9f286fac1f55a70fc
zhangjin16 <zhangjin16@kuaishou.com>
2023-08-14 17:58:30 +0800

    add some function in CLI and Server

--- SONiC_CLI/CLI.cpp
+++ SONiC_CLI/CLI.cpp
@@ -13,16 +13,17 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include "logPayload.h"
 using namespace std;
 #define SERVER_PORT 8888
-#define SERVER_IP "192.168.239.144"
+#define SERVER_IP "127.0.0.1"
 #define BUF_SIZE 1024
 
 #define EPOLL_SIZE 30
 int sigtype = 0;
 int ifexit = 0;
 
-
+int ifconnected = 0;
 void sig_int(int signo)
 {
 	sigtype = signo;
@@ -82,20 +83,6 @@ int main()
     serverAddr.sin_port = htons(SERVER_PORT);
     serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-    int connfd = socket(AF_INET,SOCK_STREAM,0);
-    if(connfd < 0)
-    {
-        printf("ERROR(line 27): create connfd failed!\n");
-        exit(-1);
-    }
-    printf("create connfd successfully!\n");
-
-    if(connect(connfd,(struct sockaddr *)&serverAddr,sizeof(serverAddr)) < 0)
-    {
-        printf("ERROR(line 34): connection to server failed!\n");
-        exit(-1);
-    }
-
     int epfd = epoll_create(EPOLL_SIZE);
     if(epfd < 0)
     {
@@ -112,27 +99,30 @@ int main()
         exit(-1);
     }
     
-    addFdToEpoll(epfd,connfd);
     addFdToEpoll(epfd,pipe_fd[0]);
-    
+    int connfd = -1; 
     int pid = fork();
     if(pid < 0)
     {
         printf("fork failed!\n");
         exit(-1);
-    }else if(pid == 0)
+    }else if(pid == 0) //子进程将控制信息写入到管道，由父进程做处理后发送到TMM-Server
     {
-        //子进程将控制信息写入到管道，由父进程做处理后发送到TMM-Server
         close(pipe_fd[0]);
         printf("Please input 'exit' to exit terminal!\n");
+        char instruction[50];
         while(!ifexit)
         {
+            bzero(&instruction,50);
+            cin.getline(instruction,50);
 
+            if(write(pipe_fd[1],instruction,strlen(instruction)) < 0)
+            {
+                printf("pipe write failed!\n");
+            }
         }
-
         close(pipe_fd[1]);
     }else{
-        
         close(pipe_fd[1]);
         while(!ifexit)
         {
@@ -156,10 +146,15 @@ int main()
                 {
                     //来自TMM-Server的日志更新信息
                     int len = -1;
+                    string data;
                     while((len = recv(sockfd,buffer,BUF_SIZE,0)) > 0)
                     {
                         //输入日志到控制台
+                        data += string(buffer);
                     }
+                    LogPayload payload;
+                    payload.parseJsonToClass(data);
+                    cout << payload.getData() << endl;
                 }else if(sockfd == pipe_fd[0])
                 {
                     //来自CLI的控制信息，需要发送到TMM-server
@@ -170,11 +165,53 @@ int main()
                     2. show terminal logging server
                     3. 构建json，构造特定的payload，包含user、pid、ip、port、报文类型、指令、日志信息、timestamp
                     */
+
                     if(res == 0){
                         printf("read control message from pipe failed!\n");
                         ifexit = 1;
                     }else{
-                        send(connfd,buffer,BUF_SIZE,0);
+                        
+                        string instruction = string(buffer);
+                        printf("收到来自子进程的消息!");
+                        cout << instruction << endl;
+                        if(instruction == "terminal logging")
+                        {
+                            if(!ifconnected)
+                            {
+                                connfd = socket(AF_INET,SOCK_STREAM,0);
+                                if(connfd < 0)
+                                {
+                                    printf("ERROR(line 27): create connfd failed!\n");
+                                    exit(-1);
+                                }
+                                printf("create connfd successfully!\n");
+
+                                if(connect(connfd,(struct sockaddr *)&serverAddr,sizeof(serverAddr)) < 0)
+                                {
+                                    printf("ERROR(line 34): connection to server failed!\n");
+                                    exit(-1);
+                                }
+
+                                addFdToEpoll(epfd,connfd);
+                                ifconnected = 1;
+
+                                LogPayload payload;
+                                payload.setType(1);//control
+                                payload.setInstruction(instruction);
+                                send(connfd,payload.toJsonString().data(),BUF_SIZE,0);             
+                            }else{
+                                printf("already start terminal logging\n");
+                            }
+                        }else if(instruction == "exit")
+                        {
+                            LogPayload payload;
+                            payload.setType(1);//control
+                            payload.setInstruction(instruction);
+                            send(connfd,payload.toJsonString().data(),BUF_SIZE,0);
+                            
+                            close(connfd);  
+                            ifexit = 1;
+                        }
                     }
                 }
             }
